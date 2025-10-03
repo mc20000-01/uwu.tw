690817d6eaecf7039186506c8db53d20a5fc4819
mc20000 <114359894+mc20000-01@users.noreply.github.com>
2025-10-03 15:08:47 -0700

    Update patch.py

--- patch.py
+++ patch.py
@@ -2,27 +2,32 @@ import os
 import glob
 import shutil
 
-for path in glob.glob('scratch-gui/build/*.html'):
-  print(f'Patching HTML {path}')
-  with open(path, 'r') as f:
-    contents = f.read()
-    contents = contents.replace('</head>', '<meta name="robots" content="noindex"></head>')
-    contents = contents.replace('<link rel="manifest" href="manifest.webmanifest">', '')
-  with open(path, 'w') as f:
-    f.write(contents)
+# Set the repo directory here
+REPO_DIR = 'mistwarp'
 
-for path in glob.glob('scratch-gui/build/**/*.js', recursive=True):
-  print(f'Patching JS {path}')
-  with open(path, 'r') as f:
-    contents = f.read()
-    contents = contents.replace('https://trampoline.turbowarp.org', 'https://trampoline.turbowarp.xyz')
-  with open(path, 'w') as f:
-    f.write(contents)
+build_dir = os.path.join(REPO_DIR, 'build')
 
-os.remove('scratch-gui/build/sw.js')
-os.remove('scratch-gui/build/manifest.webmanifest')
-os.remove('scratch-gui/build/fullscreen.html')
-os.remove('scratch-gui/build/index.html')
+for path in glob.glob(os.path.join(build_dir, '*.html')):
+    print(f'Patching HTML {path}')
+    with open(path, 'r') as f:
+        contents = f.read()
+        contents = contents.replace('</head>', '<meta name="robots" content="noindex"></head>')
+        contents = contents.replace('<link rel="manifest" href="manifest.webmanifest">', '')
+    with open(path, 'w') as f:
+        f.write(contents)
 
-shutil.copy('scratch-gui/build/editor.html', 'scratch-gui/build/index.html')
-shutil.copy('robots.txt', 'scratch-gui/build/robots.txt')
+for path in glob.glob(os.path.join(build_dir, '**', '*.js'), recursive=True):
+    print(f'Patching JS {path}')
+    with open(path, 'r') as f:
+        contents = f.read()
+        contents = contents.replace('https://trampoline.turbowarp.org', 'https://trampoline.turbowarp.xyz')
+    with open(path, 'w') as f:
+        f.write(contents)
+
+# Remove specific files
+for filename in ['sw.js', 'manifest.webmanifest', 'fullscreen.html', 'index.html']:
+    os.remove(os.path.join(build_dir, filename))
+
+# Copy files
+shutil.copy(os.path.join(build_dir, 'editor.html'), os.path.join(build_dir, 'index.html'))
+shutil.copy('robots.txt', os.path.join(build_dir, 'robots.txt'))
